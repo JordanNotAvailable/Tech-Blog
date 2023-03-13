4dea74621cd4b19d6721373ffa2f00766ea04e36
Jordan O'Grady <jord_ogrady@hotmail.com>
2023-03-13 22:48:03 +1100

    database completed with seeds and models

---
+++ seeds/Comments-seeds.js
@@ -0,0 +1,32 @@
+const { Comment } =require ('../models');
+const commentData = [
+    {
+        user_id: 3,
+        blog_id: 3,
+        comment_text: "Super Useful and helped me survive it"
+    },
+    {
+        user_id: 1,
+        blog_id: 3,
+        comment_text: "Yeah I think you need to add more points"
+    },
+    {
+        user_id: 2,
+        blog_id: 3,
+        comment_text: "Sounds like you didnt survive"
+    },
+    {
+        user_id: 2,
+        blog_id: 1,
+        comment_text: "I like the work you've done"
+    },
+    {
+        user_id: 1,
+        blog_id: 2,
+        comment_text: "Wow this guy knows what hes talking about"
+    }
+]
+
+const seedComments = () => Comment.bulkCreate(commentData);
+
+module.exports = seedComments
